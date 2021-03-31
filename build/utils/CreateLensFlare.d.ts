fe912108787a1dc0f6148e55d0aa4435fadd980e
HembitskiAndrei <shaftdg3@gmail.com>
2021-03-31 22:01:51 +0300

    optimized code

--- build/utils/CreateLensFlare.d.ts
+++
@@ -1,4 +0,0 @@
-import "@babylonjs/core/LensFlares/lensFlareSystemSceneComponent";
-import { TransformNode } from "@babylonjs/core";
-import { MainScene } from "../scenes/MainScene";
-export declare const CreateLensFlare: (scene: MainScene, node: TransformNode) => void;

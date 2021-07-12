c556dc8910949a5013e5a25739fc406e25fbca36
HembitskiAndrei <shaftdg3@gmail.com>
2021-07-12 10:36:54 +0300

    fix: gui autosize

--- build/types.d.ts
+++ build/types.d.ts
@@ -8,24 +8,24 @@ export declare type GalaxiesSceneType = MainScene;
 export declare type GalaxiesType = SpiralGalaxy | IrregularGalaxy;
 export declare type GalaxyCameraType = GalaxyCamera;
 export interface IButtonConfig {
-  width: number;
-  horizontalAlignment: number;
-  verticalAlignment: number;
-  left: number;
-  top: number;
+    width: number;
+    horizontalAlignment: number;
+    verticalAlignment: number;
+    left: number;
+    top: number;
 }
 export interface ILinesContainerConfig {
-  width: number;
-  height: number;
-  horizontalAlignment: number;
-  verticalAlignment: number;
-  left: number;
-  top: string;
+    width: number;
+    height: number;
+    horizontalAlignment: number;
+    verticalAlignment: number;
+    left: number;
+    top: string;
 }
 export interface ILineConfig {
-  width: number;
-  horizontalAlignment: number;
-  verticalAlignment: number;
-  left: number;
-  top: string;
+    width: number;
+    horizontalAlignment: number;
+    verticalAlignment: number;
+    left: number;
+    top: string;
 }
